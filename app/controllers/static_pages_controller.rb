5b1e27e63c9b093b1c3d57165f93ac7b7c813ab7
Victor <victorgutierrezgomes@gmail.com>
2020-09-05 08:47:56 -0400

    Chore: add controller methods

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,12 @@ class StaticPagesController < ApplicationController
     def index
         @nome = 'Victor Gutierrez'
     end
+
+    def sobre
+        
+    end
+
+    def contato
+        
+    end
 end
