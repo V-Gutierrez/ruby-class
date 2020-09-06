d2fbfd12ffcddeb67220ce3fdcd09437de42b067
Victor <victorgutierrezgomes@gmail.com>
2020-09-06 12:54:19 -0400

    Lesson 8

---
+++ var_const.rb
@@ -0,0 +1,17 @@
+XYZ = "I am a constant because I am all in uppercase or SNAKE_UPPER_CASE!"
+
+xyz = "I am a variable because I'm able to change and I was declared in lowercase"
+
+XYZ = "new value - It changes but it is not the right thing to do and warning is thrown"
+xyz = "I can change ! "
+
+puts XYZ, xyz
+
+####### CONSTANT CLASSES
+
+class PI
+  VALUE = 3.14
+  # a non constant value here cannot be read before initializing.
+end
+
+puts PI::VALUE  # no need to instance
